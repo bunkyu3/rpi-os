f8c156b955015ebd68304e8b3f3ce53bcab25751
bunkyu3 <pq9f07ebpr@gmail.com>
2019-06-30 00:19:00 +0900

    run 4cores and print cpuid on each core

--- exercises/3/src/kernel.c
+++ exercises/3/src/kernel.c
@@ -2,16 +2,23 @@
 #include "utils.h"
 
 void kernel_main(void){
-	uart_init();
-
 	unsigned long id;
 	id = getcpuid();
+
+	if(id == 0){
+		uart_init();
+	}else{
+		delay(100000*id);
+	}
 	uart_send_string("Hello, from processor ");
 	uart_send((char)id+0x30);
 	uart_send_string("\r\n");
 
-	while(1){
-		uart_send(uart_recv());
+	if(id == 0){
+		while(1){
+			uart_send(uart_recv());
+		}
+	}else{
+		while(1){}
 	}
 }
-
