a8e2efc4e69331544d6a223c2e5896f189dd7e98
bunkyu3 <pq9f07ebpr@gmail.com>
2019-06-29 22:18:45 +0900

    print cpuid with single core

--- exercises/3/src/kernel.c
+++ exercises/3/src/kernel.c
@@ -1,8 +1,14 @@
 #include "mini_uart.h"
+#include "utils.h"
 
 void kernel_main(void){
 	uart_init();
-	uart_send_string("Hello, world!\r\n");
+
+	unsigned long id;
+	id = getcpuid();
+	uart_send_string("Hello, from processor ");
+	uart_send((char)id+0x30);
+	uart_send_string("\r\n");
 
 	while(1){
 		uart_send(uart_recv());
